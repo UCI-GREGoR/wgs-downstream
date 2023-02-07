e16abdef9dc0c25c587d3ec0fd040103ee7d7192
Lightning Auriga <lightning.auriga@gmail.com>
2023-02-07 09:55:38 -0500

    fix: pull in upstream patches to logbook parsing
    this reflects changes in wgs-pipeline that handle discrepancies in logbook structure that have
    become apparent with older flowcell runs

--- workflow/scripts/construct_linker_from_labbook.R
+++ workflow/scripts/construct_linker_from_labbook.R
@@ -8,6 +8,44 @@ construct.output.stems <- function(df) {
   output.stems
 }
 
+#' Search for freetext ID remappings and apply them as needed
+#'
+#' The idea here is that there are evidently some freetext
+#' notes along the lines of
+#' "SAMPLE SWAP - this is actually PMGRC-\d+-\d+-\d" that
+#' only exist in freetext in rows that are labeled by
+#' _unmapped_ PMGRC ID. For downstream analysis, these ID
+#' mappings must be detected and applied. To make things
+#' even more confusing, there doesn't appear to be any sense
+#' to which columns of the logbook this information is reported in.
+#'
+#' The logic in this initial implementation will likely need to be
+#' made more complex when different upstream freetext field types
+#' are detected.
+#'
+#' @param df data.frame, input logbook sheet from read.xlsx
+#' @param vec character vector, vector of actual PMGRC IDs from
+#' current sheet
+#' @return character vector, mapped version of input character vector
+#' with ID relabelings applied
+apply.id.mappings <- function(df, vec) {
+  known.patterns <- c("^SAMPLE SWAP.*this is actually (PMGRC-\\d+-\\d+-\\d).*$" = "\\1")
+  for (i in seq_len(ncol(df))) {
+    new.ids <- rep(NA, length(vec))
+    for (j in seq_len(length(known.patterns))) {
+      has.pattern <- stringr::str_detect(df[, i], names(known.patterns)[j])
+      has.pattern[is.na(has.pattern)] <- FALSE
+      new.ids[has.pattern] <- stringr::str_replace(
+        df[has.pattern, i],
+        names(known.patterns)[j],
+        known.patterns[j]
+      )
+    }
+    vec[!is.na(new.ids)] <- new.ids[!is.na(new.ids)]
+  }
+  vec
+}
+
 run.construct.linker <- function(input.fn, output.fn) {
   sheet.names <- openxlsx::getSheetNames(input.fn)
   pmgrc.id <- c()
@@ -27,9 +65,17 @@ run.construct.linker <- function(input.fn, output.fn) {
       ls.col <- which(stringr::str_detect(colnames(df), "ls\\.id"))
       ru.col <- which(stringr::str_detect(colnames(df), "ru\\.id"))
       sex.col <- which(stringr::str_detect(colnames(df), "biological\\.sex"))
+      if (length(jira.col) == 0) {
+        jira.col <- which(stringr::str_detect(colnames(df), "^jira\\.ticket\\(s\\)\\.\\("))
+      }
       if (length(jira.col) != 1) {
         next
       }
+      ## "fix": upstream logbook has sporadic freetext annotations that indicate when a
+      ## subject needs to have an ID update applied to it. for some IDs, this information
+      ## is not recorded but rather just applied without annotation. It's not clear what
+      ## governs the difference between those two situations
+      df[, pmgrc.col] <- apply.id.mappings(df, df[, pmgrc.col])
       for (row.num in seq_len(nrow(df))) {
         pmgrc.id <- c(pmgrc.id, df[row.num, pmgrc.col])
         jira.tickets <- c(jira.tickets, df[row.num, jira.col])
