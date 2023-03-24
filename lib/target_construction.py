fa6fabcc5b05697dbce30467c1b397ec95b8be46
Lightning Auriga <lightning.auriga@gmail.com>
2023-03-24 12:56:07 -0400

    feat: add utility function to wrap remote files based on file prefix

--- lib/target_construction.py
+++ lib/target_construction.py
@@ -2,6 +2,31 @@ import os
 
 import pandas as pd
 from snakemake.io import AnnotatedString, Namedlist
+from snakemake.remote.GS import RemoteProvider as GSRemoteProvider
+from snakemake.remote.HTTP import RemoteProvider as HTTPRemoteProvider
+from snakemake.remote.S3 import RemoteProvider as S3RemoteProvider
+
+S3 = S3RemoteProvider()
+GS = GSRemoteProvider()
+HTTP = HTTPRemoteProvider()
+
+
+def annotate_remote_file(fn: str):
+    """
+    try our best to automagically wrap files with remote handlers.
+    the FTP remote handler, as of early 2023, is a bit of a mess
+    that breaks everything, so it's not included here. note that
+    rules using the S3 remote with environment SSO caching should
+    be set to high priority, lest the SSO session time out while
+    the rule is queued.
+    """
+    if fn.startswith("https://") or fn.startswith("http://"):
+        return HTTP.remote(fn)
+    if fn.startswith("gs://"):
+        return GS.remote(fn)
+    if fn.startswith("s3://"):
+        return S3.remote(fn)
+    return fn
 
 
 def map_reference_file(wildcards: Namedlist, config: dict) -> str | AnnotatedString:
