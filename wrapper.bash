abbd3d183bec72f67aa213ed8090dc8110fa555b
Lightning Auriga <lightning.auriga@gmail.com>
2023-05-19 14:27:40 -0400

    fix: restrict default number of dispatched jobs

--- wrapper.bash
+++ wrapper.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-snakemake -j250 --profile ../sge-profile -p --rerun-incomplete --rerun-triggers mtime --use-conda --use-singularity --cluster-config config/cluster.yaml --keep-going
+snakemake -j23 --profile ../sge-profile -p --rerun-incomplete --rerun-triggers mtime --use-conda --use-singularity --cluster-config config/cluster.yaml
