f452c32382291eb882a7cf13469d1314e9969a2e
Lightning Auriga <lightning.auriga@gmail.com>
2023-05-29 10:47:30 -0400

    feat: accommodate upstream pipeline changes in final bam location
    the utility script for configuring this pipeline now supports the wgs-pipeline updated final
    bam location of 'results/aligned_bams'

--- config/configure_flowcell.bash
+++ config/configure_flowcell.bash
@@ -5,6 +5,7 @@ if [[ "$#" -ne 1 ]] ; then
 fi
 
 FLOWCELL_PATH="$1"
-
-find "${FLOWCELL_PATH}/results/bqsr" -name "*.bam" -exec readlink -f {} \; | sed -r 's|^(.*)/(RU[0-9]+)/(SQ[0-9]+).bam|\2\t\3\t\1/\2/\3.bam|' >> manifest_bam.tsv
-find "${FLOWCELL_PATH}/results/deepvariant" -name "*.g.vcf.gz" -exec readlink -f {} \; | sed -r 's|^(.*)/(RU[0-9]+)/(SQ[0-9]+).sorted.g.vcf.gz|\2\t\3\t\1/\2/\3.sorted.g.vcf.gz|' >> manifest_gvcf.tsv
+## this search command was effective for wgs-pipeline version 0.3.0, but is being altered in later versions in which bqsr is optional
+## find "${FLOWCELL_PATH}/results/bqsr" -name "*.bam" -exec readlink -f {} \; | sed -r 's|^(.*)/(RU[0-9]+)/(SQ[0-9]+).bam|\2\t\3\t\1/\2/\3.bam|' >> manifest_bam.tsv
+find "${FLOWCELL_PATH}/results/aligned_bams" -name "*bam" -exec readlink -f {} \; | sed -r 's|^(.*)/([^/]+)/([^/]+).bam|\2\t\3\t\1/\2/\3.bam|' >> manifest_bam.tsv
+find "${FLOWCELL_PATH}/results/deepvariant" -name "*.g.vcf.gz" -exec readlink -f {} \; | sed -r 's|^(.*)/([^/]+)/([^/]+).sorted.g.vcf.gz|\2\t\3\t\1/\2/\3.sorted.g.vcf.gz|' >> manifest_gvcf.tsv
