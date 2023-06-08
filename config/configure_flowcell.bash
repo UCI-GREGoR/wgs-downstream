ee1883c7753f4e4a102dfcb452008b21686a0432
Lightning Auriga <lightning.auriga@gmail.com>
2023-06-08 13:58:38 -0400

    feat: update input manifests to require both gvcf and vcf files
    this should allow the ability to slice out parts of the existing callsets when appropriate

--- config/configure_flowcell.bash
+++ config/configure_flowcell.bash
@@ -8,4 +8,4 @@ FLOWCELL_PATH="$1"
 ## this search command was effective for wgs-pipeline version 0.3.0, but is being altered in later versions in which bqsr is optional
 ## find "${FLOWCELL_PATH}/results/bqsr" -name "*.bam" -exec readlink -f {} \; | sed -r 's|^(.*)/(RU[0-9]+)/(SQ[0-9]+).bam|\2\t\3\t\1/\2/\3.bam|' >> manifest_bam.tsv
 find "${FLOWCELL_PATH}/results/aligned_bams" -name "*bam" -exec readlink -f {} \; | sed -r 's|^(.*)/([^/]+)/([^/]+).bam|\2\t\3\t\1/\2/\3.bam|' >> manifest_bam.tsv
-find "${FLOWCELL_PATH}/results/deepvariant" -name "*.g.vcf.gz" -exec readlink -f {} \; | sed -r 's|^(.*)/([^/]+)/([^/]+).sorted.g.vcf.gz|\2\t\3\t\1/\2/\3.sorted.g.vcf.gz|' >> manifest_gvcf.tsv
+find "${FLOWCELL_PATH}/results/deepvariant" -name "*.g.vcf.gz" -exec readlink -f {} \; | sed -r 's|^(.*)/([^/]+)/([^/]+).sorted.g.vcf.gz|\2\t\3\t\1/\2/\3.sorted.g.vcf.gz|' | awk -F"\t" '{print $1"\t"$2"\t"$3"\t"$3}' | sed 's/.g.vcf.gz/.vcf.gz/' >> manifest_gvcf.tsv
